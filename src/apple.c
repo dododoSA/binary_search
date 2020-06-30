79d4e955fa88fda7ff35d01efa0e15998495d9f9
Akihiro Suzuki <dododosa14@gmail.com>
2020-06-30 17:12:31 +0900

    complete apple

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int m) {
+  int i;
+  long long sum;
+  sum = 0;
+  for (i = 0; i < n; i++) {
+    sum += (int)((A[i] + m -1)/m);
+  }
+  return sum <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,20 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000001;
 
+  while (ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if (p(mid)) {
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+
+  printf("%d\n", ub);
 
   return 0;
 }
