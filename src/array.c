93af9b3ef4f27448dd267ece5604f211a3e8961d
Akihiro Suzuki <dododosa14@gmail.com>
2020-06-30 16:26:43 +0900

    complete array

--- src/array.c
+++ src/array.c
@@ -4,6 +4,9 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int m) {
+  return A[m] >= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,8 +14,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = n;
 
+  while(ub - lb > 1) {
+    int mid = (lb + ub)/2;
+    if (p(mid)) {
+      ub = mid;
+    } else {
+      lb = mid;
+    }
+  }
 
-
+  printf("%d\n", ub);
   return 0;
 }
