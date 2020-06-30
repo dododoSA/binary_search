b9c51f79db695d9c3e900b5412eb9eb5c894bb54
Akihiro Suzuki <dododosa14@gmail.com>
2020-06-30 17:29:28 +0900

    complete spear

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+unsigned int p(int m) {
+  int i, sum;
+  sum = 0;
+  for (i = 0; i < n; i++) {
+    sum += (int)(A[i]/m);
+  }
+  return sum < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,20 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  lb = 0;
+  ub = 1000000001;
 
+  while (ub - lb > 1) {
+    int mid = (ub + lb) / 2;
+    if (p(mid)) {
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+
+  printf("%d\n", lb);
 
   return 0;
 }
